e4ffef5c0f03625d862caff0e3a2d39b5282f0f8
RTCupid <19apr2006@vk.com>
2025-09-12 00:19:56 +0300

    [all] + CMakeList.txt, update cache2q.hpp: + std::list<ElemT> list_in_, list_main_, and std::list<KeyT> list_out_. + 3 unordered_maps for all of queues. start make cache2q_lookup_update () to find keys in cache and update cache, + simple constructor for class cache2q_t

--- hdr/cache2q.hpp
+++ hdr/cache2q.hpp
@@ -1,20 +1,50 @@
 #ifndef CACHE2Q_H
 #define CACHE2Q_H
 
-typedef int page_id_t;
+#include <iostream>
+#include <list>
+#include <unordered_map>
 
-template <typename T>
+const float FACTOR_SIZE_LIST_IN   = 0.2;
+const float FACTOR_SIZE_LIST_MAIN = 0.2;
+const float FACTOR_SIZE_LIST_OUT  = 0.6;
+//                        sum = 1
+
+template <typename ElemT, typename KeyT>
 class cache2q_t {
     size_t size_;
 
+    std::list<ElemT> list_in_;
+    std::list<ElemT> list_main_;
+    std::list<KeyT>  list_out_;
+
+    std::unordered_map<KeyT, typename std::list<ElemT>::iterator> hash_table_in;
+    std::unordered_map<KeyT, typename std::list<ElemT>::iterator> hash_table_main;
+    std::unordered_map<KeyT, typename std::list<KeyT>::iterator>  hash_table_out;
+
 public:
-    void cache2q_lookup_update (T page);
+    cache2q_t (size_t size) : size_(size) {
+        list_in_.resize   (size * FACTOR_SIZE_LIST_IN);
+        list_main_.resize (size * FACTOR_SIZE_LIST_MAIN);
+        list_out_.resize  (size * FACTOR_SIZE_LIST_OUT);
+    }
+
+    template <typename FuncToGetElem_t>
+    void cache2q_lookup_update (KeyT key, FuncToGetElem_t the_slowest_function_to_get_elems) {
+        if (hash_table_in.find(key) != hash_table_in.end()) {
+            std::cout << "find key in hash_table_in  \n";
+        } else if (hash_table_main.find(key) != hash_table_main.end()) {
+            std::cout << "find key in hash_table_main\n";
+        } else if (hash_table_out.find(key) != hash_table_out.end()) {
+            std::cout << "find key in hash_table_out \n";
+        } else {
+            std::cout << "not found \n";
+        }
+
+    }
 };
 
-template <typename T>
-void cache2q_t<T>::cache2q_lookup_update (T page) {
-    std::cout << "Hello, world!\n";
-}
+typedef int page_id_t;
 
 struct page_t {
     page_id_t page_id;
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
+#include <cassert>
 #include "../hdr/cache2q.hpp"
 
+int the_slowest_function_to_get_int_element (int key) { return key; }
+
 int main () {
-    cache2q_t<page_t> cache2q;
+    size_t cache_size = 0;
 
-    page_t first_page = {10};
+    std::cin >> cache_size;
+    assert (std::cin.good());
 
-    cache2q.cache2q_lookup_update (first_page);
+    cache2q_t<page_t, int> cache2q(cache_size);
+
+    size_t number_elems = 0;
+    std::cin >> number_elems;
+
+    page_t first_page = {100};
+
+    cache2q.cache2q_lookup_update (number_elems, the_slowest_function_to_get_int_element);
 
     return 0;
 }
