dddbe52f9444408dc43ea6d7bd6844d1ebb9d2d4
RTCupid <19apr2006@vk.com>
2025-09-12 11:07:59 +0300

    [cache2q] some corrections in cache2q.hpp, not working code, research 2q cache, + make one hashtable for main and in queues, may be types of lists for in and main will be changed to std::pair<ElemT, KeyT>.

--- hdr/cache2q.hpp
+++ hdr/cache2q.hpp
@@ -10,6 +10,12 @@ const float FACTOR_SIZE_LIST_MAIN = 0.2;
 const float FACTOR_SIZE_LIST_OUT  = 0.6;
 //                            sum = 1
 
+enum queue_t {
+    IN,
+    MAIN,
+    OUT,
+};
+
 template <typename ElemT, typename KeyT>
 class cache2q_t {
     size_t size_;
@@ -18,8 +24,7 @@ class cache2q_t {
     std::list<ElemT> list_main_;
     std::list<KeyT>  list_out_;
 
-    std::unordered_map<KeyT, typename std::list<ElemT>::iterator> hash_table_in_;
-    std::unordered_map<KeyT, typename std::list<ElemT>::iterator> hash_table_main_;
+    std::unordered_map<KeyT, std::pair<typename std::list<ElemT>::iterator, queue_t>> hash_table_in_and_main_;
     std::unordered_map<KeyT, typename std::list<KeyT>::iterator>  hash_table_out_;
 
 public:
@@ -31,28 +36,26 @@ public:
 
     template <typename FuncToGetElem_t>
     void cache2q_lookup_update (KeyT key, FuncToGetElem_t the_slowest_function_to_get_elem_from_key) {
-        if (auto iter_key = hash_table_in_.find (key) != hash_table_in_.end ()) {
-            std::cout << "find key in hash_table_in. Continue \n";
+        auto data_from_key = hash_table_in_and_main_.find (key);
 
-        } else if ((auto iter_key = hash_table_main_.find (key)) != hash_table_main_.end ()) {
-            std::cout << "find key in hash_table_main. Process LRU algorithm...\n";
-
-            list_main_.splice (list_main_.begin (), list_main_, iter_key);
-            hash_table_main_[key] = list_main_.begin ();
-
-        } else if (auto iter_key = hash_table_out_.find (key) != hash_table_out_.end ()) {
-            std::cout << "find key in hash_table_out. Process load to main list... \n";
-
-            list_main_.push_front (the_slowest_function_to_get_elem_from_key (key));
-            hash_table_main_[key] = list_main_.begin ();
-
-            hash_table_out_.erase (key);
-            list_out_.erase (iter_key);
+        if (data_from_key != hash_table_in_and_main_.end ()) {
+            if (hash_table_in_and_main_[key].second == queue_t::MAIN) {
+                list_main_.splice (list_main_.begin (), list_main_, hash_table_in_and_main_[key].first);
+                hash_table_in_and_main_[key] = std::make_pair (list_main_.begin (), queue_t::MAIN);
+            }
         } else {
-            std::cout << "not found. Process load to list in \n";
+            auto data_from_key_out = hash_table_out_.find (key);
 
-            list_in_.push_front (the_slowest_function_to_get_elem_from_key (key));
-            hash_table_in_[key] = list_in_.begin ();
+            if (data_from_key_out != hash_table_out_.end ()) {
+                list_main_.push_front (the_slowest_function_to_get_elem_from_key (key));
+                hash_table_in_and_main_[key] = std::make_pair (list_main_.begin (), queue_t::MAIN);
+
+                hash_table_out_.erase (key);
+                list_out_.erase (data_from_key_out->second);
+            } else {
+                list_in_.push_front (the_slowest_function_to_get_elem_from_key (key));
+                hash_table_in_and_main_[key] = std::make_pair (list_main_.begin (), queue_t::IN);
+            }
         }
 
     }
