57b77acc281b5e2c4e9216646430aa3d035bfef8
RTCupid <19apr2006@vk.com>
2025-09-13 17:33:02 +0300

    [all] + debug errors in cache2q_lookup_update (), + file with colors for output, + messages to input, + counts cache hits in main, need to check overflow in queues and realize sending keys from in to out, and erase from out last element

--- hdr/cache2q.hpp
+++ hdr/cache2q.hpp
@@ -35,7 +35,7 @@ public:
     }
 
     template <typename FuncToGetElem_t>
-    void cache2q_lookup_update (KeyT key, FuncToGetElem_t the_slowest_function_to_get_elem_from_key) {
+    bool cache2q_lookup_update (KeyT key, FuncToGetElem_t the_slowest_function_to_get_elem_from_key) {
         auto data_from_key = hash_table_in_and_main_.find (key);
 
         if (data_from_key != hash_table_in_and_main_.end ()) {
@@ -43,28 +43,38 @@ public:
                 list_main_.splice (list_main_.begin (), list_main_, hash_table_in_and_main_[key].first);
                 hash_table_in_and_main_[key] = std::make_pair (list_main_.begin (), queue_t::MAIN);
             }
-        } else {
-            auto data_from_key_out = hash_table_out_.find (key);
-
-            if (data_from_key_out != hash_table_out_.end ()) {
-                list_main_.push_front (the_slowest_function_to_get_elem_from_key (key));
-                hash_table_in_and_main_[key] = std::make_pair (list_main_.begin (), queue_t::MAIN);
-
-                hash_table_out_.erase (key);
-                list_out_.erase (data_from_key_out->second);
-            } else {
-                list_in_.push_front (the_slowest_function_to_get_elem_from_key (key));
-                hash_table_in_and_main_[key] = std::make_pair (list_main_.begin (), queue_t::IN);
-            }
+            return true;
         }
 
+        auto data_from_key_out = hash_table_out_.find (key);
+
+        if (data_from_key_out != hash_table_out_.end ()) {
+            insert_to_main (key, the_slowest_function_to_get_elem_from_key (key));
+
+            erase_from_out (key, data_from_key_out->second);
+        } else {
+            insert_to_in (key, the_slowest_function_to_get_elem_from_key (key));
+        }
+        return false;
     }
+private:
+    void insert_to_in (KeyT key, ElemT new_elem_to_in) {
+        list_in_.push_front (new_elem_to_in);
+        hash_table_in_and_main_[key] = std::make_pair (list_in_.begin (), queue_t::IN);
+    }
+
+    void insert_to_main (KeyT key, ElemT new_elem_to_main) {
+        list_main_.push_front (new_elem_to_main);
+        hash_table_in_and_main_[key] = std::make_pair (list_main_.begin (), queue_t::MAIN);
+    }
+
+    void erase_from_out (KeyT key, typename std::list<KeyT>::iterator iterator_list_out) {
+        hash_table_out_.erase (key);
+        list_out_.erase (iterator_list_out);
+    }
+
 };
 
-typedef int page_id_t;
-
-struct page_t {
-    page_id_t page_id;
-};
+typedef int page_t;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,42 @@
 #include <iostream>
 #include <cassert>
-#include "../hdr/cache2q.hpp"
 
-int the_slowest_function_to_get_int_element (int key) { return key; }
+#include "../hdr/cache2q.hpp"
+#include "../colors.h"
+
+page_t the_slowest_function_to_get_int_element (int key) { return key; }
 
 int main () {
+    std::cout << GRN "### realization of 2Q cache\n" RESET;
+    std::cout << GRN "# (c) RTCupid, 2024\n\n" RESET;
+
     size_t cache_size = 0;
 
+    std::cout << BLU "Enter cache size: " YEL;
     std::cin >> cache_size;
     assert (std::cin.good());
 
     cache2q_t<page_t, int> cache2q(cache_size);
 
     size_t number_elems = 0;
+
+    std::cout << BLU "Enter number of elements: " YEL;
     std::cin >> number_elems;
 
-    page_id_t page_id = 0;
+    page_t page_id = 0;
+    size_t hits    = 0;
 
     for (size_t i = 0; i < number_elems; i++) {
-        std::cin >> page_id;
+        std::cout << BLU "Enter " << i << " element: " YEL;
+        std::cin  >> page_id;
 
-        cache2q.cache2q_lookup_update (page_id, the_slowest_function_to_get_int_element);
+        if (cache2q.cache2q_lookup_update (page_id, the_slowest_function_to_get_int_element)) {
+            std::cout << GRN "cache hit " YEL << page_id << std::endl;
+            hits++;
+        }
     }
+
+    std::cout << GRN "### number of hits = " YEL << hits << std::endl;
 
     return 0;
 }
